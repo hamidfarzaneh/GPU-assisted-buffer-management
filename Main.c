19ab1d3c23658130ae146e2db9daa775d3594f5a
hamidfarzaneh <hamidfarzaneh75@gmail.com>
2018-03-10 09:45:00 +0330

    Trying to make a buffer manager for just main memory with LRU policy

--- Main.c
+++ Main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <unistd.h> 
 
+int PAGE_SIZE ;
 int main(int argc , char ** argv){
 
-    printf("Hello world!\n");
+	sysconf(_SC_PAGESIZE);
+    PAGE_SIZE = sysconf(_SC_PAGE_SIZE);
+    printf("current page size %d Bytes\n" , PAGE_SIZE);
+    char * myBufferPtr = " Greetings , Professor Falken .\ n " ;
+
+    printf("%016p\n" , &(myBufferPtr));
     return 0 ;
 }
